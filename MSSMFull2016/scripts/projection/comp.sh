3759f7c722e6744b97e1bac05eee9491f40f5ea7
mflechl <martin.flechl@cern.ch>
2018-07-03 16:48:15 +0200

    fixing bugs adding scen2

--- MSSMFull2016/scripts/projection/comp.sh
+++ MSSMFull2016/scripts/projection/comp.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 declare -A ds
 
-d='output/' 
-ds_nosys=( ${d}'2018-05-28_17h12m52_lumi-36.9_nosyst/'    ${d}'2018-05-28_21h31m42_lumi-300.0_nosyst/'    ${d}'2018-05-29_06h49m00_lumi-3000.0_nosyst/' )
-ds_nosca=( ${d}'2018-05-28_17h26m20_lumi-36.9/'           ${d}'2018-05-28_21h45m09_lumi-300.0/'           ${d}'2018-05-29_07h02m42_lumi-3000.0/' )
-ds_scall=( ${d}'2018-05-29_14h11m23_lumi-36.9_scale-all/' ${d}'2018-05-29_16h20m33_lumi-300.0_scale-all/' ${d}'2018-05-29_13h29m05_lumi-3000.0_scale-all/' )
-ds_scbbb=( ${d}'2018-05-28_19h03m17_lumi-36.9_scale-bbb/' ${d}'2018-05-29_01h57m17_lumi-300.0_scale-bbb/' ${d}'2018-05-29_17h45m29_lumi-3000.0_scale-bbb/' )
+d='output/'
+tag='jun27_'
+
+ds_nosys=( ${d}${tag}'lumi-36.9_nosyst/'    ${d}${tag}'lumi-300.0_nosyst/'    ${d}${tag}'lumi-3000.0_nosyst/' )
+ds_nosca=( ${d}${tag}'lumi-36.9/'           ${d}${tag}'lumi-300.0/'           ${d}${tag}'lumi-3000.0/' )
+ds_scall=( ${d}${tag}'lumi-36.9_scale-all/' ${d}${tag}'lumi-300.0_scale-all/' ${d}${tag}'lumi-3000.0_scale-all/' )
+ds_scbbb=( ${d}${tag}'lumi-36.9_scale-bbb/' ${d}${tag}'lumi-300.0_scale-bbb/' ${d}${tag}'lumi-3000.0_scale-bbb/' )
+ds_scsc2=( ${d}${tag}'lumi-36.9_scale-scen2/' ${d}${tag}'lumi-300.0_scale-scen2/' ${d}${tag}'lumi-3000.0_scale-scen2/' )
 o='output/limit_comp' 
 
 
-modes=( 'no_systematics' 'no_scaling'  'scale_all' 'scale_bbb' )
+modes=( 'no_systematics' 'no_scaling'  'scale_all' 'scale_bbb' 'scale_scen2' )
 lumi=(  '36.9_fb^{-1}'   '300_fb^{-1}' '3000_fb^{-1}'          )
 ds[no_systematics]=ds_nosys[@]
 ds[no_scaling]=ds_nosca[@]
 ds[scale_all]=ds_scall[@]
 ds[scale_bbb]=ds_scbbb[@]
+ds[scale_scen2]=ds_scsc2[@]
 
 for m in ${modes[@]}; do 
     i=0
@@ -28,16 +32,19 @@ for m in ${modes[@]}; do
 	    lterm+=${ds_[$i]}${p}'_cmb.json:exp0:Title='"\"${lumi[$i]}\""
 	    lterm+=" "
 	done
-#	echo $lterm
-	python scripts/plotMSSMLimits.py --logy --logx --show exp0 $lterm --cms-sub="Preliminary" -o ${o}/${m}_${p} --process=${p:0:2}'#phi' --title-right="13 TeV" --use-hig-17-020-style --auto-style --ratio-to ${ds_[0]}${p}'_cmb.json:exp0' --title-left="$title_left"
+	echo Using $lterm
+	python scripts/plotMSSMLimits.py --logy --logx --show exp0 $lterm --cms-sub="Internal" -o ${o}/${m}_${p} --process=${p:0:2}'#phi' --title-right="13 TeV" --use-hig-17-020-style --auto-style --ratio-to ${ds_[0]}${p}'_cmb.json:exp0' --title-left="$title_left"
     done
 done
 
 for l in ${lumi[@]}; do 
     lumititle=${l/"_fb^{-1}"/""}
     i=0
-    for m in ${modes[@]}; do for d in ${!ds[$m]}; do if [[ $d =~ $lumititle ]]; then ds_[i]=$d; let i=$i+1; fi; done; done
-#    echo ${ds_[@]}
+    echo lumititle $lumititle
+    for m in ${modes[@]}; do for d in ${!ds[$m]}; do 
+	    if [[ "$d" =~ "${lumititle}." ]] || [[ "$d" =~ "${lumititle}_" ]] || [[ "$d" =~ "${lumititle}/" ]]; then ds_[i]=$d; let i=$i+1; fi; done; 
+    done
+    echo XX ${ds_[@]}
 
     lumititle=${lumititle/"."/"p"}
     for p in 'ggH' 'bbH'; do
@@ -48,8 +55,8 @@ for l in ${lumi[@]}; do
 	    lterm+=${ds_[$i]}${p}'_cmb.json:exp0:Title='"\"${modes[$i]}\""
 	    lterm+=" "
 	done
-#	echo $lterm
-	python scripts/plotMSSMLimits.py --logy --logx --show exp0 $lterm --cms-sub="Preliminary" -o ${o}/${lumititle}fb_${p} --process=${p:0:2}'#phi' --title-right="13 TeV" --use-hig-17-020-style --auto-style --ratio-to ${ds_[0]}${p}'_cmb.json:exp0' --title-left="$title_left"
+	echo Using $lterm
+	python scripts/plotMSSMLimits.py --logy --logx --show exp0 $lterm --cms-sub="Internal" -o ${o}/${lumititle}fb_${p} --process=${p:0:2}'#phi' --title-right="13 TeV" --use-hig-17-020-style --auto-style --ratio-to ${ds_[0]}${p}'_cmb.json:exp0' --title-left="$title_left"
     done
 done
 
