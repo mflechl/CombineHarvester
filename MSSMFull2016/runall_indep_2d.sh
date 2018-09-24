7a0991a1171f839eeaa62f3ba0d79d1e9e3997b6
mflechl <martin.flechl@cern.ch>
2018-09-24 08:53:21 +0200

    2d run file added

---
+++ MSSMFull2016/runall_indep_2d.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+lumis=( 35.9 )
+odir="sep21"
+
+modes="all" #model-indep
+#modes="allnp" #model-indep
+#modes="mlfit np" #model-indep
+#extra=""
+extra="--symdir latest --dim2"
+
+for l in "${lumis[@]}"; do
+    echo "###############################"
+#    run.py $modes --lumi $l --outdir $odir  --nosyst $extra
+    echo "###############################"
+#   run.py $modes --lumi $l --outdir $odir --scale scen2 $extra
+    echo "###############################"
+#    run.py $modes --lumi $l --outdir $odir --scale all $extra
+    echo "###############################"
+#    run.py $modes --lumi $l --outdir $odir --scale bbb $extra
+    echo "###############################"
+    time run.py $modes --lumi $l --outdir $odir $extra
+    echo "###############################"
+#    run.py $modes --lumi $l --outdir $odir --scale scen2 --bbb 0.0 $extra
+done
